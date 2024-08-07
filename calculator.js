69812ac4521450a576de636fc1de0d430e521aa6
dinosawrfish <dinosawrfish@gmail.com>
2024-08-06 17:45:47 -0700

    add selectNumber function and listener to create number

--- calculator.js
+++ calculator.js
@@ -1,4 +1,26 @@
-const testNumber = 400;
+const total = 0;
 
 const output = document.querySelector(".output");
-output.innerText = testNumber;
+const numbers = document.querySelector(".numbers");
+
+
+// What does a calculator do
+
+// TODO: output to show number selected
+function selectNumber(output, input) {
+    let newNumber = "";
+
+    if (output === "0") {
+        newNumber = input;
+    } else {
+        newNumber = output + input;
+    }
+    return newNumber;
+}
+// TODO: output add number to new decimal place when output not zero
+// TODO: current output needs to follow selected operand with selected number
+
+// listeners
+numbers.addEventListener("click", function(event) {
+    output.innerText = selectNumber(output.innerText, event.target.innerText);
+});
