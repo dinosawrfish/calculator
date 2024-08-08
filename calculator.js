a7650629a4c9f468a7970b18a9e78105b429ba1b
dinosawrfish <dinosawrfish@gmail.com>
2024-08-07 18:37:31 -0700

    add event listener for clear button, resets calculator

--- calculator.js
+++ calculator.js
@@ -1,7 +1,4 @@
 let total = 0;
-let num1 = 0;
-let num2 = 0;
-
 let operation = null;
 
 const output = document.querySelector(".output");
@@ -92,4 +89,10 @@ operands.addEventListener("click", function(event) {
 
 backspace.addEventListener("click", function() {
     output.innerText = removeRightDigit(output.innerText);
+})
+
+clear.addEventListener("click", function() {
+    output.innerText = 0;
+    total = 0;
+    operation = null;
 })
